079302161d697828a1e85566a88841a60217dab3
Ron Salama <156225488+Ron-Salama@users.noreply.github.com>
2025-03-22 20:47:01 +0200

    Time: 5 ms (32.96%) | Memory: 12.4 MB (86.01%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -13,7 +13,28 @@ class Solution(object):
         
         for char in range(abs(sign),len(s)):
             if(s[char].isdigit()):
-                result_as_integer = result_as_integer*10 + int(s[char])
+                if s[char] == '0':
+                    temp = 0
+                elif s[char] == '1':
+                    temp = 1
+                elif s[char] == '2':
+                    temp = 2
+                elif s[char] == '3':
+                    temp = 3
+                elif s[char] == '4':
+                    temp = 4
+                elif s[char] == '5':
+                    temp = 5
+                elif s[char] == '6':
+                    temp = 6
+                elif s[char] == '7':
+                    temp = 7
+                elif s[char] == '8':
+                    temp = 8
+                elif s[char] == '9':
+                    temp = 9
+                            
+                result_as_integer = result_as_integer*10 + int(temp)
             else:
                 break
         if(sign == 0):
