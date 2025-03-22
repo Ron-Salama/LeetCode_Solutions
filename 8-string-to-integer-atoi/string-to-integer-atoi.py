3cd9cc93e1f5967ebe55642c45c063002834b763
Ron Salama <156225488+Ron-Salama@users.noreply.github.com>
2025-03-22 20:40:37 +0200

    Time: 0 ms (100.00%) | Memory: 12.4 MB (86.01%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -3,33 +3,47 @@ class Solution(object):
         result_as_integer = 0
         s = s.strip()
         sign = 0
-        if s == "":
+        if(s==""):
             return 0
         
-        if s[0] == "-":
+        if(s[0]=="-"):
             sign = -1
-        elif s[0] == "+":
+        elif(s[0]=="+"):
             sign = 1
-
-        digit_map = {
-            '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
-            '5': 5, '6': 6, '7': 7, '8': 8, '9': 9
-        }
-
-        for char in range(abs(sign), len(s)):
-            if s[char] in digit_map:
-                result_as_integer = result_as_integer * 10 + digit_map[s[char]]
+        
+        for char in range(abs(sign),len(s)):
+            if(s[char].isdigit()):
+                if s[char] == '0':
+                    temp = 0
+                elif s[char] == '1':
+                    temp = 1
+                elif s[char] == '2':
+                    temp = 2
+                elif s[char] == '3':
+                    temp = 3
+                elif s[char] == '4':
+                    temp = 4
+                elif s[char] == '5':
+                    temp = 5
+                elif s[char] == '6':
+                    temp = 6
+                elif s[char] == '7':
+                    temp = 7
+                elif s[char] == '8':
+                    temp = 8
+                elif s[char] == '9':
+                    temp = 9
+                            
+                result_as_integer = result_as_integer*10 + temp
             else:
                 break
-
-        if sign == 0:
+        if(sign == 0):
             sign = 1
+        result_as_integer = result_as_integer*sign
 
-        result_as_integer = result_as_integer * sign
-
-        if result_as_integer > 2**31 - 1:
+        if(result_as_integer>2**31 - 1):
             result_as_integer = 2**31 - 1
-        elif result_as_integer < -2**31:
+        elif(result_as_integer< -2**31):
             result_as_integer = -2**31
 
         return result_as_integer
